8a042bdb9d5ba0aa5c6b0b4a156e4d7764be8ae0
gabe <gabriel.debeaupuis@gmail.com>
2016-01-25 14:24:36 -0800

    Added Load More Button to append data to ember-table

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  size: 10,
+  offset: 0,
   model() {
     return Ember.RSVP.hash({
-      users: this.store.findAll('user'),
+      users: this.store.query('user', { size: this.get('size') }),
     });
   },
 
@@ -13,4 +14,14 @@ export default Ember.Route.extend({
     controller.set('model', model);
   },
 
+  actions: {
+    loadMore() {
+      this.set('offset', this.get('offset') + this.get('size'));
+      this.store.query('user', {
+        size: this.get('size'),
+        offset: this.get('offset')
+      });
+      this.set('controller.model.users', this.store.peekAll('user'));
+    },
+  },
 });
